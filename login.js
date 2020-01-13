ee8bffb9ccb4063c62331094b5c39f16e990a57a
mengru <4momolmr@gmail.com>
2020-01-14 00:21:36 +0800

    -5

---
+++ login.js
@@ -0,0 +1,13 @@
+var loginbtn = document.querySelector("#loginbtn");
+var username = document.querySelector("#username");
+var password = document.querySelector("#password");
+
+loginbtn.addEventListener('click', function () {
+	console.log(username.value);
+   if(username.value !=="user0115" || password.value!=="answeriswords"){
+	   alert("請輸入正確的帳號或密碼");
+   } else {
+	window.location = "https://ruila.github.io/game/ready.html"
+   }
+   
+});
